af183c781dd25c4b8b9629c5b1cec483336f7066
Thyago Quintas <thyagoquintas@hotmail.com>
2012-10-03 17:06:05 -0700

    Change the way to call the function
    Change the way to call the function

--- jquery.storage.js
+++ jquery.storage.js
@@ -2,6 +2,7 @@
  * jQuery Storage plugin v1.0.2
  * 		v1.0.1 - Fix bug when jQuery try read value to radio or checkbox;
  *		v1.0.2 - Change for Array the return value
+ *      v1.0.3 - Change the way to call the function
  * 
  * Copyright 2012, Thyago Quintas (dev@thyagoquintas.com.br)
  * https://github.com/thyagoquintas/jquery-storage/
@@ -11,69 +12,61 @@
  * http://www.opensource.org/licenses/GPL-2.0 
  */
 
-;(function($){
+(function ($, document, undefined) {
 
   var methods = {
+  
     init : function(content) {
-	    var objects = [];
+	    if(content == undefined) $.error('Content is empty on jQuery.storage');
+	    if(content['key'] == undefined) $.error('You need the key value');
 	    
-	    // Return the values in stored
-    	this.each(function(){
-	    	var contents = $.extend({
-				    'name' : $(this).attr("id"),
-				    'location' : $(this).storage('support') ? 'localStorage':'cookies'
-			}, content);
-			var object = [];
-			
-			if(contents['location'] == 'localStorage'){
-		    	object.push(contents['name']);
-		    	object.push(localStorage[contents['name']]);
-	        }else if(contents['location'] == 'cookie') {
-		        object.push(contents['name']);
-		        object.push($.cookie(contents['name']));
-	        }else {
-		        $.error('Location not accepted.');
-	        }
-	        objects.push(object);
-		});
-		return objects;	
+	    var contents = $.extend({
+			    'location' : $.storage('support') ? 'localStorage':'cookies'
+		}, content);
+
+		if(contents['location'] == 'localStorage'){
+			return localStorage[contents['key']];
+	    }else if(contents['location'] == 'cookie') {
+			return $.cookie(contents['key']);
+	    }else {
+			$.error('Location not accepted.');
+	    }
     },//Close return
     
     // Remove the values in stored
     remove : function(content) {
-	    this.each(function(){
-		    var contents = $.extend({
-			    'name' : $(this).attr("id"),
-			    'location' : $(this).storage('support') ? 'localStorage':'cookies'
-			}, content);
+	    if(content == undefined) $.error('Content is empty on jQuery.storage');
+	    if(content['key'] == undefined) $.error('You need the key value');
+		    
+		var contents = $.extend({
+			'location' : $.storage('support') ? 'localStorage':'cookies'
+		}, content);
 
-		    if(contents['location'] == 'localStorage'){
-		    	localStorage.removeItem(contents['name']);
-	        }else if(contents['location'] == 'cookie') {
-		        $.removeCookie((contents['name']));
-	        }else {
-		        $.error('Location not accepted.');
-	        }
-	    });
+		if(contents['location'] == 'localStorage'){
+			localStorage.removeItem(contents['key']);
+	    }else if(contents['location'] == 'cookie') {
+			$.removeCookie((contents['key']));
+	    }else {
+			$.error('Location not accepted.');
+	    }
     },//Close remove
     
     // Update the values in stored
     update : function(content) {
-    	this.each(function(){
-	    	var contents = $.extend( {
-	          'value' : (($(this).attr('type') == "checkbox") || ($(this).attr('type') == "radio")) ?  $(this).is(':checked') : $(this).val(),
-	          'name' : $(this).attr("id"),
-	          'location' : $(this).storage('support') ? 'localStorage':'cookies'
-	        }, content);
-	        
-	        if(contents['location'] == 'localStorage'){
-		        localStorage[contents['name']] = contents['value'];
-	        }else if(contents['location'] == 'cookie') {
-		        $.cookie(contents['name'], contents['value']);
-	        }else {
-		        $.error('Location not accepted.');
-	        }
-	    });
+		if(content == undefined) $.error('Content is empty on jQuery.storage');
+	    if(content['key'] == undefined) $.error('You need the key value');
+		
+		var contents = $.extend( {
+	    	'location' : $.storage('support') ? 'localStorage':'cookies'
+	    }, content);
+	    
+	    if(contents['location'] == 'localStorage'){
+		    localStorage[contents['key']] = JSON.stringify(contents['value']);
+	    }else if(contents['location'] == 'cookie') {
+		     $.cookie(contents['key'], JSON.stringify(contents['value']));
+	    }else {
+		    $.error('Location not accepted.');
+	    }
     },//Close update
     
     // Verify the compatibility with localStorage
@@ -82,7 +75,7 @@
     }
   };
 
-  $.fn.storage = function(method) {
+  $.storage = function(method) {
     if (methods[method]) {
       return methods[method].apply( this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === 'object' || ! method) {
@@ -91,4 +84,4 @@
       $.error('Method ' +  method + ' does not exist on jQuery.storage');
     }
   };
-})(jQuery);
+})(jQuery, document);
